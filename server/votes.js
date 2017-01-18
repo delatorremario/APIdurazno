750c8feca1a8c7ee7971a5e8c53c0a9eb065bf09
Mario de la Torre <mariodelatorre@MBP-de-Mario.iptv.microsoft.com>
2017-01-18 17:09:32 -0300

    votes add

--- server/votes.js
+++ server/votes.js
@@ -1,48 +1,24 @@
 
-Router.route('/votes',{where: 'server'})
-  .get(function(){
-        var response = Artist.find().fetch();
-        this.response.setHeader('Content-Type','application/json');
-        this.response.end(JSON.stringify(response));
-  })
+Router.route('/vote',{where: 'server'})
   .post(function(){
         var response;
-        if(this.request.body.vote === undefined) {
+        if(this.request.body.artist === undefined) {
             response = {
                 "error" : true,
                 "message" : "invalid data"
             };
         } else {
-            Artist.update({_id:vote},{
-                name : this.request.body.name,
-                _id : this.request.body._id
-            });
+               var vote = this.request.body;
+
+          var result = Artist.update({name:vote.artist,'songs.name':vote.song},{
+                 $addToSet:{'songs.$.votes':vote.idfbk}
+                }
+            );
             response = {
                 "error" : false,
-                "message" : "Artist added."
+                "message" : "Vote added."
             }
         }
         this.response.setHeader('Content-Type','application/json');
         this.response.end(JSON.stringify(response));
   });
-
-Router.route('/votes/:id',{where: 'server'})
-
-    // GET /message/:id - returns specific records
-
-    .get(function(){
-        var response;
-        if(this.params.id !== undefined) {
-            var data = Artist.find({_id : this.params.id}).fetch();
-            if(data.length > 0) {
-                response = data
-            } else {
-                response = {
-                    "error" : true,
-                    "message" : "Artist not found."
-                }
-            }
-        }
-        this.response.setHeader('Content-Type','application/json');
-        this.response.end(JSON.stringify(response));
-    });
