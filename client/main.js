b24190362dfd2cb6979d4aa3631856e77802c230
Mario de la Torre <mariodelatorre@MBP-de-Mario.iptv.microsoft.com>
2017-01-24 18:26:15 -0300

    song rows

--- client/main.js
+++ client/main.js
@@ -3,22 +3,31 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.home.onCreated(function helloOnCreated() {
+
+
+
+
+Template.artist.events({
+  'click button'(event, instance) {
+    // hacer el sorteo
+    var winning_song = _.max(this.songs,function(song){return song.votescount});
+    console.log('winning_song',winning_song);
+
+    var idfbk = winning_song.votes[_.random(0,winning_song.votes.length -1)]
+    var fbkuser = FacebookUser.findOne({public_profile:idfbk});
+    console.log(idfbk,fbkuser);
+    instance.winner.set(fbkuser || 'Perfil no encontrado');
+  },
+});
+
+Template.artist.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.winner = new ReactiveVar();
 });
 
-Template.home.helpers({
+
+Template.artist.helpers({
     winner () {
-      return Template.instance().winner.get()
-    }
-});
-
-
-Template.home.events({
-  'click button'(event, instance) {
-    // hacer el sorteo
-    var fbkusers = this.fbkusers.fetch();
-    instance.winner.set(fbkusers[_.random(0,fbkusers.length -1)]);
-  },
-});
+       return Template.instance().winner.get()
+   }
+})
